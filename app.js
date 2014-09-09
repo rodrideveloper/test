fefd4222e2aa950bb1b522e0311bc0c1793592b7
Rodrigo Rodriguez <rodrigo.rodriguez@live.com.ar>
2014-09-09 11:16:50 -0300

    new files

---
+++ app.js
@@ -0,0 +1,13 @@
+var express=require('express');
+var app=express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req, res){
+  res.render('index.html');
+});
+
+
+app.listen(3000,function(res,res){   
+	console.log('Server Corriendo en puerto ' + 3000);
+});
